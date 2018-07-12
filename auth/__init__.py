349ff84e85a9e69545b1f535a317ccb6c4b682b4
Artem Tiumentcev <darland.maik@gmail.com>
2018-07-12 20:00:44 +0300

    Added class to verify user authentication

--- auth/__init__.py
+++ auth/__init__.py
@@ -2,12 +2,12 @@ import ujson
 import aiohttp
 import jwt
 
+
 from aiohttp_boilerplate.views.exceptions import JSONHTTPError
 from aiohttp_boilerplate.config import config
 
 
 async def validate_token(token):
-
     if token is None or len(token) < 100:
         raise JSONHTTPError(
             {"__error__": "Authentication"},
@@ -27,3 +27,34 @@ async def validate_token(token):
 
 def encode_token(token):
     return jwt.decode(token, verify=False)
+
+
+class Auth:
+
+    async def auth_user(self, is_superadmin=False):
+        self.user = await validate_token(
+            self.request.headers.get('Authorization')
+        )
+
+        if is_superadmin:
+            await self.check_permission()
+
+    async def try_auth_user(self):
+        if not hasattr(self, 'user'):
+            try:
+                await self.auth_user()
+            except (aiohttp.web.HTTPUnauthorized, aiohttp.web.HTTPForbidden):
+                pass
+        return hasattr(self, 'user')
+
+    async def is_user(self):
+        return await self.try_auth_user()
+
+    async def is_admin(self):
+        if await self.try_auth_user():
+            return self.user.get('is_superuser', False)
+        return False
+
+    async def check_permission(self):
+        if not await self.is_admin():
+            raise JSONHTTPError({'error': 'Forbidden'}, aiohttp.web.HTTPForbidden)
