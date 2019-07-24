555d9aca085ec41882353483a01941c6a80ed886
Artem Tiumentcev <darland.maik@gmail.com>
2019-07-24 14:49:56 +0300

    Prepare setup file to publish pkg

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
+import pathlib
+import re
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as fp:
-    install_requires = fp.read()
+name = 'aiohttp-boilerplate'
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+here = pathlib.Path(__file__).parent
+
+txt = (here / 'aiohttp_boilerplate' / '__init__.py').read_text('utf-8')
+install_requires = (here / 'requirements.txt').read_text('utf-8')
+long_description = (here / 'README.md').read_text('utf-8')
+
+try:
+    version = re.findall(r"^__version__ = '([^']+)'\r?$", txt, re.M)[0]
+except IndexError:
+    raise RuntimeError('Unable to determine version.')
 
 setup(
-    name='aiohttp_boilerplate',
-    version='',
+    name=name,
+    version=version,
     install_requires=install_requires,
     packages=find_packages(),
-    license='',
-    author='webdeveloppro',
-    author_email='',
-    description=long_description
+    license='MIT',
+    author='Pro Webdevelop LLC',
+    author_email='vladka@webdevelop.pro',
+    description=long_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/webdeveloppro/aiohttp_boilerplate',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
