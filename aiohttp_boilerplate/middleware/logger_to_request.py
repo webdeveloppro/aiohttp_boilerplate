0ae443e46ce696efe2a3137e363377ff95c54bc1
George <georgy.schapchits@synesis.ru>
2023-12-28 13:22:13 +0300

    Update logger format (#29)
    * Update logs format
    
    * Update aiohttp_boilerplate/config/__init__.py
    
    Co-authored-by: Vladyslav Tarasenko <vladka@webdevelop.pro>
    
    * Update __init__.py
    
    removed unused line
    
    ---------
    
    Co-authored-by: Vladyslav Tarasenko <vladka@webdevelop.pro>

--- aiohttp_boilerplate/middleware/logger_to_request.py
+++ aiohttp_boilerplate/middleware/logger_to_request.py
@@ -2,23 +2,16 @@ import logging
 from pythonjsonlogger import jsonlogger
 from aiohttp import web
 from ..logging.helpers import GCPLogger
+from aiohttp_boilerplate.logging import get_logger
 
 
 # Create logger in request with all info
 @web.middleware
 async def logger_to_request(request:web.Request, handler):
-    log = GCPLogger("request")
-
-    set_json_formatter(log)
-
+    log = get_logger("request")
     log.setRequest(request)
+
     request.log = log
     response = await handler(request)
     log.setResponse(response)
     return response
-
-def set_json_formatter(log):
-    logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
-    logHandler.setFormatter(formatter)
-    log.addHandler(logHandler)
