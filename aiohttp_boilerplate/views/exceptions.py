d5e321d4a36b71a546af64621379560eff8ebbcc
Vlad <vladka@webdevelop.pro>
2024-05-23 16:17:54 +0300

    refactor jsonhttperror class

--- aiohttp_boilerplate/views/exceptions.py
+++ aiohttp_boilerplate/views/exceptions.py
@@ -11,7 +11,8 @@ logger = get_logger(logger_name)
 class JSONHTTPError(web.HTTPClientError):
     def __init__(self, request, message, error_class=web.HTTPBadRequest):
         """ Helper to parse json and return string """
-        request.log.debug(message)
+        if request is not None:
+            request.log.debug(message)
         self.request = request
         self.status_code = error_class().status_code
 
